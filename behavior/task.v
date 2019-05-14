bd5383a17b1884645533338b5dbde4e859a3b31d
Björn Brandenburg <bbb@mpi-sws.org>
2019-05-14 17:28:45 +0200

    port scheduler_executes_job_with_earliest_arrival

--- behavior/task.v
+++ behavior/task.v
@@ -1,3 +1,4 @@
+From mathcomp Require Export ssrbool.
 From rt.behavior Require Export job.
 
 (* Throughout the library we assume that jobs have decidable equality *)
@@ -6,4 +7,17 @@ Definition TaskType := eqType.
 
 (* Definition of a generic type of parameter relating jobs to tasks *)
 
-Class JobTask (T : TaskType) (J : JobType) := job_task : J -> T.
+Class JobTask (J : JobType) (T : TaskType) := job_task : J -> T.
+
+
+Section SameTask.
+  (* For any type of job associated with any type of tasks... *)
+  Context {Job: JobType}.
+  Context {Task: TaskType}.
+  Context `{JobTask Job Task}.
+
+  (* ... we say that two jobs j1 and j2 are from the same task iff job_task j1
+     is equal to job_task j2. *)
+  Definition same_task j1 j2 := job_task j1 == job_task j2.
+
+End SameTask.
