bffcc219ef228565ff232cebda042eac31bb1319
Björn Brandenburg <bbb@mpi-sws.org>
2019-07-18 00:12:52 +0200

    note two simple facts about job deadlines

--- restructuring/behavior/facts/all.v
+++ restructuring/behavior/facts/all.v
@@ -3,3 +3,4 @@ Require Export rt.restructuring.behavior.facts.completion.
 Require Export rt.restructuring.behavior.facts.ideal_schedule.
 Require Export rt.restructuring.behavior.facts.sequential.
 Require Export rt.restructuring.behavior.facts.arrivals.
+Require Export rt.restructuring.behavior.facts.deadlines.
