bbab834c5b75710ed05597e7960e6c4db800e861
Sergei Bozhko <sbozhko@mpi-sws.org>
2019-10-23 15:07:37 +0200

    Move workload lemma to basic files

--- restructuring/analysis/basic_facts/all.v
+++ restructuring/analysis/basic_facts/all.v
@@ -1,6 +1,8 @@
 Require Export rt.restructuring.analysis.basic_facts.service.
+Require Export rt.restructuring.analysis.basic_facts.service_of_jobs.
 Require Export rt.restructuring.analysis.basic_facts.completion.
 Require Export rt.restructuring.analysis.basic_facts.ideal_schedule.
 Require Export rt.restructuring.analysis.basic_facts.sequential.
 Require Export rt.restructuring.analysis.basic_facts.arrivals.
 Require Export rt.restructuring.analysis.basic_facts.deadlines.
+Require Export rt.restructuring.analysis.basic_facts.workload.
