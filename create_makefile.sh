8037703de6764039bcf9488c8a1bf6ea1baefee0
Björn Brandenburg <bbb@mpi-sws.org>
2019-11-12 15:58:36 +0100

    cleanup create_makefile.sh
    1) rely on patch to actually patch the Makefile
    2) move ineffective 'sed' hackery related to 'make validate'
    
    Re 2), the sed script didn't actually modify the Makefile anyway
    (unlear how long it has been ineffective), so let's just remove it.

--- create_makefile.sh
+++ create_makefile.sh
@@ -25,18 +25,7 @@ FIND_OPTS+=( -print )
 # Compile all relevant *.v files
 coq_makefile -f _CoqProject $(find "${FIND_OPTS[@]}" ) -o Makefile
 
-# Fix the 'make validate' command. It doesn't handle the library prefix properly
-# and cause clashes for files with the same name. This forces full filenames and
-# adds the rt. prefix
-if [ "$(uname)" == "Darwin" ]; then
-	sed -i '' 's|$(notdir $(^:.vo=))|$(addprefix rt., $(subst /,., $(^:.vo=)))|g' Makefile
-else
-	sed -i 's|$(notdir $(^:.vo=))|$(addprefix rt., $(subst /,., $(^:.vo=)))|g' Makefile
-fi
-
-# Patch Makefile.coqdocjs for pretty documentation targets
-printf "\n# Include pretty documentation targets\ninclude scripts/coqdocjs/Makefile.coqdocjs" >> Makefile
-
 # Patch HTML target to switch out color, and 
 # so that it parses comments and has links to ssreflect.
+# Also include Makefile.coqdocjs for 'htmlpretty' documentation target.
 patch -s < scripts/Makefile.patch
