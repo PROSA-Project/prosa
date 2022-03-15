26911c641c2ee7daa479951c604b9a182fdb1006
Björn Brandenburg <bbb@mpi-sws.org>
2022-03-15 16:24:55 +0100

    add sporadic task -> arrival curve interpretation

---
+++ model/task/arrival/sporadic_to_arrival_curve.v
@@ -0,0 +1,51 @@
+Require Export prosa.util.all.
+Require Export prosa.model.task.arrival.curves.
+Require Export prosa.analysis.facts.sporadic.arrival_bound.
+
+(** Arrival Curve for Sporadic Tasks *)
+
+(** Any analysis that supports arbitrary arrival curves can also be
+    used to analyze sporadic tasks. We establish this link below. *)
+Section SporadicArrivalCurve.
+  (** Consider any type of sporadic tasks. *)
+  Context {Task : TaskType} `{SporadicModel Task}.
+
+  (** The bound on the maximum number of arrivals in a given interval
+      [max_sporadic_arrivals] is in fact a valid arrival curve, which
+      we note with the following arrival-curve declaration. *)
+  Global Program Instance MaxArrivalsSporadic : MaxArrivals Task := max_sporadic_arrivals.
+
+  (** It remains to be shown that [max_sporadic_arrivals] satisfies
+      all expectations placed on arrival curves. First, we observe
+      that the bound is structurally sound. *)
+  Lemma sporadic_arrival_curve_valid :
+    forall tsk,
+      valid_arrival_curve (max_sporadic_arrivals tsk).
+  Proof.
+    move=> tsk; split; first exact: div_ceil0.
+    move=> delta1 delta2 LEQ.
+    exact: div_ceil_monotone1.
+  Qed.
+
+   (** Next, we note that it is indeed an arrival bound. To this end,
+       consider any type of jobs stemming from these tasks ... *)
+  Context {Job : JobType} `{JobTask Job Task} `{JobArrival Job}.
+
+  (** ... and any well-formed arrival sequence of such jobs. *)
+  Variable arr_seq : arrival_sequence Job.
+  Hypothesis H_consistent_arrivals: consistent_arrival_times arr_seq.
+  Hypothesis H_uniq_arr_seq: arrival_sequence_uniq arr_seq.
+
+  (** Let [tsk] denote any valid sporadic task to be analyzed. *)
+  Variable tsk : Task.
+  Hypothesis H_sporadic_model: respects_sporadic_task_model arr_seq tsk.
+  Hypothesis H_valid_inter_min_arrival: valid_task_min_inter_arrival_time tsk.
+
+  (** We observe that [max_sporadic_arrivals] is indeed an upper bound
+      on the maximum number of arrivals. *)
+  Lemma sporadic_arrival_curve_respects_max_arrivals :
+    respects_max_arrivals arr_seq tsk (max_sporadic_arrivals tsk).
+  Proof. move=> t1 t2 LEQ. exact: sporadic_task_arrivals_bound. Qed.
+
+End SporadicArrivalCurve.
+
