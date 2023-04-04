246dc9d0b1a37efd9a1877abd51e235e8b948e6f
Björn Brandenburg <bbb@mpi-sws.org>
2023-04-04 19:13:23 +0200

    revise ELF<->FP generality claim
    1) Remove infeasible hypothesis.
    
    2) Observe that ELF always is an FP schedule, without any further
       assumptions.
    
    3) Prove that FP->ELF under strengthened assumption (task
       sequentiality, schedule and arrival sequence validity).

--- results/elf/generality.v
+++ results/elf/generality.v
@@ -1,8 +1,10 @@
 From mathcomp Require Import eqtype ssrnat seq path fintype bigop.
 Require Export prosa.util.int.
 Require Export prosa.model.schedule.priority_driven.
-Require Export prosa.model.priority.gel.
 Require Export prosa.model.priority.elf.
+Require Export prosa.analysis.facts.priority.gel.
+Require Export prosa.analysis.facts.priority.classes.
+Require Export prosa.analysis.facts.model.sequential.
 
 (** * Generality of ELF *)
 
@@ -29,44 +31,10 @@ Section GeneralityOfELF.
   (** Suppose [fp] is the fixed-priority policy that parameterizes the ELF policy. *)
   Variable fp: FP_policy Task.
 
-  (** ** ELF Generalizes Fixed-Priority Scheduling *)
-
-  (** First, let us consider fixed-priority scheduling, which by design
-      ELF trivially generalizes. *)
-
-  Section ELFGeneralizesFixedPriority.
-
-    (** If each task has a distinct priority, or equivalently, no two tasks
-        have equal priority according to the [fp] fixed-priority policy,... *)
-    Hypothesis H_FP_policy_is_distinct:
-      forall tsk1 tsk2, ~~ ep_task tsk1 tsk2.
-
-    (** ... then the ELF policy reduces to the underlying FP policy. *)
-    Remark elf_generalizes_fixed_priority :
-      forall sched arr_seq,
-        respects_JLFP_policy_at_preemption_point arr_seq sched (ELF fp)
-        <-> respects_FP_policy_at_preemption_point arr_seq sched fp.
-    Proof.
-      move=> sched arr_seq; split => RESPECTED j j' t ARR PT BL SCHED;
-        rewrite /hep_job_at /FP_to_JLFP /JLFP_to_JLDP /hep_job.
-      - move: (RESPECTED j j' t ARR PT BL SCHED);
-          rewrite /hep_job_at /JLFP_to_JLDP /ELF /hep_job
-          => /orP[/andP[HEP NOTHEP] //| /andP[HEP GEL] //].
-      rewrite /ELF; apply /orP; left.
-      move: (RESPECTED j j' t ARR PT BL SCHED);
-        rewrite /hep_job_at /FP_to_JLFP /JLFP_to_JLDP /hep_job => HEP.
-      specialize (H_FP_policy_is_distinct (job_task j) (job_task j')).
-      move: H_FP_policy_is_distinct;
-        rewrite /ep_task negb_and => /orP[NOTHEP| NOTHEP].
-      - by apply /andP; split.
-      - case: (negP NOTHEP HEP).
-    Qed.
-
-  End ELFGeneralizesFixedPriority.
-
   (** ** ELF Generalizes GEL *)
 
-  (** ELF similarly generalizes GEL in a trivial manner. *)
+  (** First, let us consider GEL scheduling, which by design ELF trivially
+      generalizes. *)
   Section ELFGeneralizesGEL.
 
   (** If the [fp] fixed-priority policy assigns equal priority to all tasks... *)
@@ -91,6 +59,78 @@ Section GeneralityOfELF.
       move: H_FP_policy_is_same; rewrite /ep_task =>/andP[_ HEP].
       by apply /andP; split. }
   Qed.
+
+  (** ** ELF Generalizes Fixed-Priority Scheduling *)
+
+  (** Next, let us turn to fixed-priority scheduling, which by design ELF also
+      generalizes under certain assumptions. *)
+
+  Section ELFGeneralizesFixedPriority.
+
+    (** Since (1) ELF uses GEL only to break ties in fixed task priority, and
+        since (2) a fixed-priority policy says nothing about how jobs of
+        equal-priority tasks should be ordered (i.e., "ties in priority are
+        broken arbitrarily"), an [ELF fp] schedule always is also a valid [fp]
+        schedule. *)
+    Remark elf_is_fixed_priority :
+      forall sched arr_seq,
+        respects_JLFP_policy_at_preemption_point arr_seq sched (ELF fp)
+        -> respects_FP_policy_at_preemption_point arr_seq sched fp.
+    Proof.
+      move=> sched arr_seq RESPECTED j j' t ARR PT BL SCHED.
+      rewrite /hep_job_at /FP_to_JLFP /JLFP_to_JLDP /hep_job.
+      by move: (RESPECTED j j' t ARR PT BL SCHED)
+         ; rewrite /hep_job_at /JLFP_to_JLDP /ELF /hep_job
+            => /orP[/andP[HEP NOTHEP] //| /andP[HEP GEL] //].
+    Qed.
+
+    (** Additionally, if each task has a distinct priority, or equivalently, no
+        two tasks have equal priority according to the [fp] fixed-priority
+        policy, then the reverse also holds: the ELF policy reduces to the
+        underlying FP policy. To show this, we require some additional setup and
+        assumptions. *)
+
+    (** First, assume that task priorities are indeed distinct. *)
+    Hypothesis H_distinct_fixed_priorities:
+      forall tsk1 tsk2, tsk1 != tsk2 -> ~~ ep_task tsk1 tsk2.
+
+    (** Second, consider any given valid arrival sequence.*)
+    Variable arr_seq : arrival_sequence Job.
+    Hypothesis H_valid_arrival_sequence : valid_arrival_sequence arr_seq.
+
+    (** Then in any given valid schedule ... *)
+    Variable sched : schedule PState.
+    Hypothesis H_sched_valid : valid_schedule sched arr_seq.
+
+    (** ... in which tasks execute sequentially ... *)
+    Hypothesis H_sequential : sequential_tasks arr_seq sched.
+
+    (** ... the ELF policy and the underlying FP policy are equivalent. *)
+    Remark elf_generalizes_fixed_priority :
+        respects_JLFP_policy_at_preemption_point arr_seq sched (ELF fp)
+        <-> respects_FP_policy_at_preemption_point arr_seq sched fp.
+    Proof.
+      split; first exact: elf_is_fixed_priority.
+      move=> RESPECTED j j' t ARR PT BL SCHED.
+      move: (RESPECTED j j' t ARR PT BL SCHED);
+        rewrite /hep_job_at /FP_to_JLFP /JLFP_to_JLDP /hep_job => HEP.
+      rewrite /ELF; apply /orP.
+      have [/eqP EQ|NEQ] := eqVneq (job_task j') (job_task j); [right|left].
+      { apply/andP; split => //.
+        have [LEQ|LT] := leqP (job_arrival j') (job_arrival j);
+          first by rewrite hep_job_arrival_gel.
+        exfalso.
+        have COMP: completed_by sched j t
+          by apply: (H_sequential j j'); rt_eauto => //; rewrite same_task_sym.
+        move: BL. rewrite /backlogged => /andP [INCOMP _].
+        apply ready_implies_incomplete in INCOMP.
+        by move: INCOMP => /negP. }
+      { have /negP NEP: ~~ ep_task (job_task j') (job_task j)
+          by apply: H_distinct_fixed_priorities.
+        by move: HEP; rewrite hep_hp_ep_task => /orP [|]. }
+    Qed.
+
+  End ELFGeneralizesFixedPriority.
 
 End ELFGeneralizesGEL.
 
