10fe88167db70058687046d5532cf9d61e749f48
Sergei Bozhko <sbozhko@mpi-sws.org>
2025-06-12 22:36:21 +0200

    prove a few properties of [blackout_during]

--- analysis/facts/behavior/supply.v
+++ analysis/facts/behavior/supply.v
@@ -72,6 +72,16 @@ Section UnitService.
     by rewrite /is_blackout /has_supply /supply_at; case: supply_in => [|[]].
   Qed.
 
+  (** Vice versa, [is_blackout t] is equal to [1 - supply_at t], up to
+      the [bool]–[nat] conversion. *)
+  Lemma is_blackout_complement :
+    forall t,
+      nat_of_bool (is_blackout sched t) = 1 - supply_at sched t.
+  Proof.
+    move=> t; have := H_unit_supply_proc_model (sched t).
+    by rewrite /is_blackout /has_supply /supply_at; case: supply_in => [|[]].
+  Qed.
+
   (** We note that supply is bounded at all times by [1] ... *)
   Remark supply_at_le_1 :
     forall t,
@@ -99,6 +109,15 @@ Section UnitService.
     by apply: leq_sum => t0 _;  apply supply_at_le_1.
   Qed.
 
+  (** We show that blackout during an interval <<[t, t + δ)>> is bounded by [δ]. *)
+  Lemma blackout_during_bound :
+    forall t δ,
+      blackout_during sched t (t + δ) <= δ.
+  Proof.
+    move=> t d; rewrite -[leqRHS](addKn t) -[leqRHS]muln1 -sum_nat_const_nat.
+    by apply: leq_sum => t0 _; destruct is_blackout.
+  Qed.
+
   (** We observe that the supply during an interval can be decomposed
       into the last instant and the rest of the interval. *)
   Lemma supply_during_last_plus_before :
@@ -107,9 +126,30 @@ Section UnitService.
       supply_during sched t1 t2.+1 = supply_during sched t1 t2 + supply_at sched t2.
   Proof. by move => t1 t2 LEQ; rewrite /supply_during big_nat_recr //=. Qed.
 
-  (** Finally, we show that the blackout during a time interval <<[t,
-      t + δ)>> is equal to the difference between [δ] and the supply
-      during <<[t, t + δ)>>. *)
+  (** We show a similar lemma for [blackout_during]. *)
+  Lemma blackout_during_last_plus_before :
+    forall t1 t2,
+      t1 <= t2 ->
+      blackout_during sched t1 t2.+1 = blackout_during sched t1 t2 + is_blackout sched t2.
+  Proof. by move => t1 t2 LEQ; rewrite /blackout_during big_nat_recr //=. Qed.
+
+  (** We show that the service during a time interval <<[t, t + δ)>>
+      is equal to the difference between [δ] and the blackout during
+      <<[t, t + δ)>>. *)
+  Lemma supply_during_complement :
+    forall t δ,
+      supply_during sched t (t + δ) = δ - blackout_during sched t (t + δ).
+  Proof.
+    move=> t; elim=> [|δ IHδ]; first by rewrite addn0 [LHS]big_geq.
+    rewrite addnS [LHS]big_nat_recr ?leq_addr//= [X in X + _]IHδ.
+    rewrite blackout_during_last_plus_before ?leq_addr// is_blackout_complement.
+    have B := supply_at_le_1 (t + δ).
+    by rewrite -addn1 subnACA ?blackout_during_bound; lia.
+  Qed.
+
+  (** Similarly, we show that the blackout during a time interval
+      <<[t, t + δ)>> is equal to the difference between [δ] and the
+      supply during <<[t, t + δ)>>. *)
   Lemma blackout_during_complement :
     forall t δ,
       blackout_during sched t (t + δ) = δ - supply_during sched t (t + δ).
@@ -118,6 +158,36 @@ Section UnitService.
     rewrite addnS [LHS]big_nat_recr ?leq_addr//= [X in X + _]IHδ.
     rewrite supply_during_last_plus_before ?leq_addr// supply_at_complement.
     by rewrite -addn1 subnACA ?supply_during_bound; lia.
+  Qed.
+
+  (** The number of blackout time instants in <<[t1, t2)>> is equal to
+      the sum of blackout time instants in <<[t1, t)>> and <<[t, t2)>>. *)
+  Lemma blackout_during_cat :
+    forall (t1 t2 t : instant),
+      t1 <= t <= t2 ->
+      blackout_during sched t1 t + blackout_during sched t t2 = blackout_during sched t1 t2.
+  Proof.
+    move=> t1 t2 t NEQ; rewrite /blackout_during.
+    rewrite (index_iota_cat t t1 t2) => //.
+    by rewrite big_cat //=.
+  Qed.
+
+  (** The number of blackout time instants can increase by at most 1
+      when extending the interval by one unit. *)
+  Lemma blackout_during_unit_growth :
+    forall (t : instant),
+      unit_growth_function (blackout_during sched t).
+  Proof.
+    move=> t1 t2.
+    have UNIT :
+      forall (f : nat -> nat) (t1 t2 : instant),
+        (f t2 <= 1) ->
+        \sum_(t1 <= t < t2 + 1) f t <= \sum_(t1 <= t < t2) f t + 1.
+    { clear; intros f t1 t2; have [LE|LT] := leqP t1 t2.
+      { by rewrite addn1 big_nat_recr //= leq_add2l. }
+      { by rewrite big_geq // addn1. }
+    }
+    by apply UNIT; destruct is_blackout.
   Qed.
 
 End UnitService.
