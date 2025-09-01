62440198f336833bbba619b1139d7e26d48bdab8
Sergey Bozhko <sbozhko@mpi-sws.org>
2025-09-01 23:25:16 +0200

    define notion of all arrivals having posit. costs

--- model/job/properties.v
+++ model/job/properties.v
@@ -14,3 +14,25 @@ Section PropertiesOfJob.
   Definition job_cost_positive := job_cost j > 0.
 
 End PropertiesOfJob.
+
+(** In the next section, we lift the above property to the level 
+    of arrival sequences. *)
+Section PropertiesOfAllJobs.
+
+  (** Consider any type of jobs... *)
+  Context {Job : JobType}.
+
+  (** ... and the cost associated with each job. *)
+  Context `{JobCost Job}.
+
+  (** Next, consider any arrival sequence of such jobs. *)
+  Variable arr_seq : arrival_sequence Job.
+
+  (** We say that the arrival sequence has positive job costs if every
+      job in the sequence has positive cost. *)
+  Definition arrivals_have_positive_job_costs :=
+    forall j,
+      arrives_in arr_seq j ->
+      job_cost_positive j.
+
+End PropertiesOfAllJobs.
