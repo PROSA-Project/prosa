fed10a5613cf938c0ba7a7abd73f147b0b71df36
Björn Brandenburg <bbb@mpi-sws.org>
2025-09-03 10:26:00 +0200

    scripts: run `ruff` on Python code

--- scripts/extract-comments.py
+++ scripts/extract-comments.py
@@ -2,13 +2,14 @@
 
 import argparse
 import sys
-import os
 import re
 
-INLINE_CODE_RE = re.compile(r'\[[^]]*?\]')
+INLINE_CODE_RE = re.compile(r"\[[^]]*?\]")
+
 
 def comment_ranges(src):
     "Identify comments in Coq .v files."
+
     def cur_is(i, c):
         return src[i] == c
 
@@ -25,60 +26,72 @@ def comment_ranges(src):
     for i in range(len(src)):
         assert in_comment >= 0
         # comment starting?
-        if cur_is(i, '(') and next_is(i, '*'):
+        if cur_is(i, "(") and next_is(i, "*"):
             in_comment += 1
             if in_comment == 1:
-                if next_is(i + 1, '*'):
+                if next_is(i + 1, "*"):
                     comment_start = i + 3
                 else:
                     comment_start = i + 2
         # comment ending?
-        elif cur_is(i, '*') and next_is(i, ')'):
+        elif cur_is(i, "*") and next_is(i, ")"):
             in_comment -= 1
             if in_comment == 0:
                 yield (comment_start, i)
 
 
 def process(opts, fname):
-    src = open(fname, 'r').read()
+    src = open(fname, "r").read()
     if opts.merge_dots:
         if opts.keep_inline:
             comments = [src[a:b].strip() for (a, b) in comment_ranges(src)]
         else:
-            comments = [INLINE_CODE_RE.sub('', src[a:b].strip()) for (a, b) in comment_ranges(src)]
+            comments = [
+                INLINE_CODE_RE.sub("", src[a:b].strip())
+                for (a, b) in comment_ranges(src)
+            ]
         merged_comments = []
         for c in comments:
             if not merged_comments:
                 merged_comments.append(c)
-            if merged_comments[-1].endswith('...') and c.startswith('...'):
+            if merged_comments[-1].endswith("...") and c.startswith("..."):
                 merged_comments[-1] = f"{merged_comments[-1][:-3]}{c[3:]}"
             else:
-               merged_comments.append(c)
+                merged_comments.append(c)
 
         for c in merged_comments:
             print(c)
     else:
-        for (a, b) in comment_ranges(src):
+        for a, b in comment_ranges(src):
             txt = src[a:b]
             if opts.keep_inline:
                 print(txt)
             else:
-                print(INLINE_CODE_RE.sub('', txt))
+                print(INLINE_CODE_RE.sub("", txt))
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description="extract comments from Gallina files")
+    parser = argparse.ArgumentParser(description="extract comments from Gallina files")
 
-    parser.add_argument('input_files', nargs='*',
-        metavar='Gallina-file',
-        help='input Gallina files (*.v)')
-    parser.add_argument('--keep-inline', action='store_true',
-        help='Do not strip inline code from comments')
-    parser.add_argument('--merge-dots', action='store_true',
-        help='Merge continued comments follwing the "..." pattern.')
-
+    parser.add_argument(
+        "input_files",
+        nargs="*",
+        metavar="Gallina-file",
+        help="input Gallina files (*.v)",
+    )
+    parser.add_argument(
+        "--keep-inline",
+        action="store_true",
+        help="Do not strip inline code from comments",
+    )
+    parser.add_argument(
+        "--merge-dots",
+        action="store_true",
+        help='Merge continued comments follwing the "..." pattern.',
+    )
 
     return parser.parse_args()
+
 
 def main():
     opts = parse_args()
@@ -95,5 +108,6 @@ def main():
         # signal something went wrong with non-zero exit code
         sys.exit(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- scripts/proofloc.py
+++ scripts/proofloc.py
@@ -8,44 +8,50 @@ import sys
 from collections import defaultdict
 
 SINGLE_LINE_PROOF_PATTERN = re.compile(
-r"""
+    r"""
 ([ \t]|^)Proof\.  # Proof keyword
 .*             # The actual proof.
 [ \t.]Qed\.   # End of proof keyword.
-"""
-, re.VERBOSE)
+""",
+    re.VERBOSE,
+)
 
 PROOF_START_PATTERN = re.compile(r"[ \t^](Proof|Next +Obligation)\.")
 PROOF_END_PATTERN = re.compile(r"[ \t^.](Qed|Defined)\.")
 
 CLAIM_NAME_PATTERN = re.compile(
-r"""
+    r"""
 # first a keyword indicating a claim
 (Lemma|Theorem|Corollary|Fact|Remark|Example)
 \s+       # whitespace
 ([^:\s]+) # the identifier (anything up to the first space or colon)
-"""
-, re.VERBOSE)
+""",
+    re.VERBOSE,
+)
 
 COMMENT_START_PATTERN = re.compile(r"\(\*")
 COMMENT_END_PATTERN = re.compile(r"\*\)")
 
+
 def banner(fname):
     fill1 = (76 - len(fname)) // 2
     fill2 = 76 - len(fname) - fill1
-    print("%s[ %s ]%s" % ('=' * fill1, fname, '=' * fill2))
+    print("%s[ %s ]%s" % ("=" * fill1, fname, "=" * fill2))
+
 
 def show_line(tag, line_number, line):
-    print("%3s %4d %s" % (tag, line_number + 1, line), end='')
+    print("%3s %4d %s" % (tag, line_number + 1, line), end="")
+
 
 def silent(tag, line_number, line):
     pass
+
 
 def process_file(fname, annotate=silent):
     proofs = []
     in_proof = False
     in_comment = 0
-    claim = '???'
+    claim = "???"
     with open(fname) as f:
         for i, line in enumerate(f):
             # simplification: we assume comment start/end not on otherwise
@@ -54,28 +60,29 @@ def process_file(fname, annotate=silent):
             in_comment -= len(COMMENT_END_PATTERN.findall(line))
             assert in_comment >= 0
             name_match = CLAIM_NAME_PATTERN.search(line)
-            if not name_match is None:
+            if name_match is not None:
                 claim = name_match.group(2)
             if in_proof:
                 if not in_comment and PROOF_END_PATTERN.search(line):
-                    annotate('###', i, line)
-                    proofs.append( (fname, start + 1, claim, i - start - 1) )
+                    annotate("###", i, line)
+                    proofs.append((fname, start + 1, claim, i - start - 1))
                     in_proof = False
-                    claim = '???'
+                    claim = "???"
                 else:
-                    annotate(' P ', i, line)
+                    annotate(" P ", i, line)
             elif not in_comment and SINGLE_LINE_PROOF_PATTERN.search(line):
-                annotate('*P*', i, line)
-                proofs.append( (fname, i + 1, claim, 1) )
-                claim = '???'
+                annotate("*P*", i, line)
+                proofs.append((fname, i + 1, claim, 1))
+                claim = "???"
             elif not in_comment and PROOF_START_PATTERN.search(line):
-                annotate('***', i, line)
+                annotate("***", i, line)
                 in_proof = True
                 start = i
             else:
-                annotate('   ', i, line)
+                annotate("   ", i, line)
 
     return proofs
+
 
 def show_histogram(opts, all_proofs):
     limit = 0
@@ -90,18 +97,19 @@ def show_histogram(opts, all_proofs):
     for i, count in enumerate(counts):
         print("  <= %3d  %7d" % ((i + 1) * opts.bin_size, count))
 
+
 def create_db(opts, all_proofs):
     db = {}
     db["manual_exceptions"] = opts.long_proofs["manual_exceptions"]
     db["legacy_proofs"] = defaultdict(dict)
     for fname, line, claim, length in all_proofs:
-        if fname in db["manual_exceptions"] and \
-            claim in db["manual_exceptions"][fname]:
-                # skip manually approved long proofs
-                continue
+        if fname in db["manual_exceptions"] and claim in db["manual_exceptions"][fname]:
+            # skip manually approved long proofs
+            continue
         if length >= opts.threshold:
             db["legacy_proofs"][fname][claim] = length
     print(json.dumps(db, sort_keys=True, indent=4))
+
 
 def check_proof_lengths(opts, all_proofs):
     new_long_proofs = False
@@ -111,25 +119,33 @@ def check_proof_lengths(opts, all_proofs):
             break
         known = False
         for category in ["manual_exceptions", "legacy_proofs"]:
-            if fname in opts.long_proofs[category] and \
-                claim in opts.long_proofs[category][fname] and \
-                    opts.long_proofs[category][fname][claim] >= length:
-                        # this is a known long proof of non-increased length
-                        # no need to complain
-                        known = True
+            if (
+                fname in opts.long_proofs[category]
+                and claim in opts.long_proofs[category][fname]
+                and opts.long_proofs[category][fname][claim] >= length
+            ):
+                # this is a known long proof of non-increased length
+                # no need to complain
+                known = True
         if not known:
-            print("Warning: new long proof of %s in %s:%d!"
-                    % (claim, fname, line), file=sys.stderr)
+            print(
+                "Warning: new long proof of %s in %s:%d!" % (claim, fname, line),
+                file=sys.stderr,
+            )
             new_long_proofs = True
         else:
             known_long_proofs = known_long_proofs + 1
     if known_long_proofs:
-        print("Checked %d proofs in %d files, while skipping %d known long proofs."
-                % (len(all_proofs), len(opts.input_files), known_long_proofs))
+        print(
+            "Checked %d proofs in %d files, while skipping %d known long proofs."
+            % (len(all_proofs), len(opts.input_files), known_long_proofs)
+        )
     else:
-        print("Checked %d proofs in %d files."
-                % (len(all_proofs), len(opts.input_files)))
+        print(
+            "Checked %d proofs in %d files." % (len(all_proofs), len(opts.input_files))
+        )
     return new_long_proofs
+
 
 def rank_proofs(opts, all_proofs):
     print("%12s   %s" % ("Proof LOC", "Location (Claim)"))
@@ -139,40 +155,71 @@ def rank_proofs(opts, all_proofs):
             break
         print("%12d   %s:%d (%s)" % (length, fname, line, claim))
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description="Proof counting tool")
+    parser = argparse.ArgumentParser(description="Proof counting tool")
 
-    parser.add_argument('input_files', nargs='*',
-        metavar='Coq-file',
-        help='input Coq files (*.v)')
+    parser.add_argument(
+        "input_files", nargs="*", metavar="Coq-file", help="input Coq files (*.v)"
+    )
 
-    parser.add_argument('-t', '--threshold', default=40,
-                        action='store', type=int,
-                        help='up to which length is a proof is considered ok')
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        default=40,
+        action="store",
+        type=int,
+        help="up to which length is a proof is considered ok",
+    )
 
-    parser.add_argument('-b', '--bin-size', default=None,
-                        action='store', type=int, metavar="BIN-SIZE",
-                        help='report length histogram with given bin size')
+    parser.add_argument(
+        "-b",
+        "--bin-size",
+        default=None,
+        action="store",
+        type=int,
+        metavar="BIN-SIZE",
+        help="report length histogram with given bin size",
+    )
 
-    parser.add_argument('--extract', default=False, action='store_true',
-                        help='simply list all identified proofs')
+    parser.add_argument(
+        "--extract",
+        default=False,
+        action="store_true",
+        help="simply list all identified proofs",
+    )
 
-    parser.add_argument('--annotate', default=False, action='store_true',
-                        help='dump the file with proofs marked')
+    parser.add_argument(
+        "--annotate",
+        default=False,
+        action="store_true",
+        help="dump the file with proofs marked",
+    )
 
-    parser.add_argument('--create-db', default=False, action='store_true',
-                        help='create a DB of known long proofs (JSON format)')
+    parser.add_argument(
+        "--create-db",
+        default=False,
+        action="store_true",
+        help="create a DB of known long proofs (JSON format)",
+    )
 
-    parser.add_argument('--long-proofs', default=None,
-                        type=argparse.FileType('r'), metavar="JSON-FILE",
-                        help='JSON DB of known long proofs')
+    parser.add_argument(
+        "--long-proofs",
+        default=None,
+        type=argparse.FileType("r"),
+        metavar="JSON-FILE",
+        help="JSON DB of known long proofs",
+    )
 
-    parser.add_argument('--check', default=False, action='store_true',
-                        help='complain and return non-zero exit code if there '
-                             'are new too-long proofs')
+    parser.add_argument(
+        "--check",
+        default=False,
+        action="store_true",
+        help="complain and return non-zero exit code if there are new too-long proofs",
+    )
 
     return parser.parse_args()
+
 
 def main():
     opts = parse_args()
@@ -180,7 +227,7 @@ def main():
     if opts.long_proofs:
         opts.long_proofs = json.load(opts.long_proofs)
     else:
-        opts.long_proofs = { "manual_exceptions" : {}, "legacy_proofs" : {} }
+        opts.long_proofs = {"manual_exceptions": {}, "legacy_proofs": {}}
 
     all_proofs = []
     annotate = show_line if opts.annotate else silent
@@ -203,6 +250,6 @@ def main():
     else:
         rank_proofs(opts, all_proofs)
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
