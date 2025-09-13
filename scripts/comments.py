dd79dcb88c932e37a4512d224857b77ccc6d3980
Björn Brandenburg <bbb@mpi-sws.org>
2025-09-13 13:02:31 +0200

    add `scripts/prosa-linter.py`

--- scripts/comments.py
+++ scripts/comments.py
@@ -1,4 +1,9 @@
-def comment_ranges(src):
+from bisect import bisect
+from collections.abc import Iterable, Iterator
+from functools import cached_property
+
+
+def comment_ranges(src) -> Iterator[tuple[int, int]]:
     "Identify comments in Gallina files (i.e., Rocq's .v files)."
 
     def cur_is(i, c):
@@ -11,7 +16,7 @@ def comment_ranges(src):
             return False
 
     in_comment = 0
-    comment_start = None
+    comment_start = 0
 
     # limitation: doesn't do anything smart about nested comments for now
     for i in range(len(src)):
@@ -29,3 +34,96 @@ def comment_ranges(src):
             in_comment -= 1
             if in_comment == 0:
                 yield (comment_start, i)
+
+
+def line_ranges(src) -> Iterator[tuple[int, int]]:
+    "Yield a list of offset pairs indicating all lines in the input"
+    last = 0
+    _line_ranges = []
+    for i, c in enumerate(src):
+        if c == "\n":
+            yield (last, i + 1)
+            last = i + 1
+    if last < len(src):
+        yield (last, len(src))
+
+
+class Ranges:
+    def __init__(self, ranges: Iterable[tuple[int, int]]):
+        self.ranges = tuple(ranges)
+
+    def lookup(self, pos: int | tuple[int, int]) -> tuple[int, tuple[int, int]] | None:
+        s = pos[0] if isinstance(pos, tuple) else pos
+        e = pos[1] if isinstance(pos, tuple) else pos + 1
+        x = bisect(self.ranges, s, key=lambda r: r[1])
+        for i, r in enumerate(self.ranges[x:]):
+            if r[0] <= s < r[1] and r[0] < e <= r[1]:
+                return (i + x), r
+            if s >= r[1]:
+                break
+        return None
+
+    def __iter__(self) -> Iterator[tuple[int, int]]:
+        return iter(self.ranges)
+
+    def __getitem__(self, pos: int | tuple[int, int]) -> tuple[int, int] | None:
+        where = self.lookup(pos)
+        if where is not None:
+            return where[1]
+        else:
+            return
+
+    def __contains__(self, pos: int | tuple[int, int]) -> bool:
+        return self.lookup(pos) is not None
+
+
+class Comments:
+    def __init__(self, src):
+        self.src = src
+
+    @cached_property
+    def ranges(self) -> Ranges:
+        return Ranges(comment_ranges(self.src))
+
+    def __iterator__(self) -> Iterator[str]:
+        return (self.src[s:e] for s, e in self.ranges)
+
+    def __contains__(self, pos):
+        return pos in self.ranges
+
+
+class LineNumbers:
+    def __init__(self, src):
+        self.src = src
+
+    @cached_property
+    def ranges(self) -> Ranges:
+        return Ranges(line_ranges(self.src))
+
+    def lines(self) -> Iterator[str]:
+        return (self.src[s:e] for s, e in self.ranges)
+
+    def line_containing(self, pos: int) -> tuple[int, tuple[int, int]]:
+        where = self.ranges.lookup(pos)
+        if where is not None:
+            return where
+        else:
+            raise IndexError(f"{pos} is not a valid file offset")
+
+    def line_number_for_offset(self, pos: int) -> int:
+        where = self.line_containing(pos)
+        return where[0] + 1
+
+    def line_for_offset(self, pos: int) -> str:
+        where = self.line_containing(pos)
+        return self.src[where[1][0] : where[1][1]]
+
+    def offset_within_line(self, pos: int) -> int:
+        where = self.line_containing(pos)
+        return pos - where[1][0]
+
+    def __getitem__(self, pos: int) -> int:
+        return self.line_number_for_offset(pos)
+
+    def __contains__(self, pos: int) -> bool:
+        return pos in self.ranges
---
+++ scripts/prosa-linter.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+from comments import Comments, LineNumbers
+
+ISSUES = [
+    (re.compile(regex, re.MULTILINE | re.DOTALL), msg, adjust_edit_offset)
+    for msg, regex, adjust_edit_offset in [
+        (
+            "missing space before ':='",
+            r"(Lemma|Theorem|Fact|Corollary|Remark|Definition|Fixpoint)\s+[^.]*?(\S:=)[^.]*?\.",
+            1,
+        ),
+        (
+            "missing space after ':='",
+            r"(Lemma|Theorem|Fact|Corollary|Remark|Definition|Fixpoint)\s+[^.]*?(:=\S)[^.]*?\.",
+            2,
+        ),
+        (
+            "missing space before ':'",
+            r"(Hypothesis|Variable|Variables|Instance|Context)\s+[^.]*?(\S:)\s[^.]*?\.",
+            1,
+        ),
+        (
+            "missing space after ':'",
+            r"(Hypothesis|Variable|Variables|Instance|Context)\s+[^.]*?([^. \n]:[^= \n])[^.]*?\.",
+            2,
+        ),
+        (
+            "missing space before ':'",
+            r"(Lemma|Theorem|Fact|Corollary|Remark|Definition|Fixpoint)\s+[^.]*?(\S:)\s[^.]*?(:=)?[^.]*?\.",
+            1,
+        ),
+        (
+            "missing space after ':'",
+            r"(Lemma|Theorem|Fact|Corollary|Remark|Definition|Fixpoint)\s+[^.]*?(:[^= \n])[^.]*?:=[^.]*?\.",
+            1,
+        ),
+    ]
+]
+
+EXCEPTIONS = [
+    r"%:R",  # MathComp syntax for nat -> ring coercion
+    ("", r"[::", "]"),  # MathComp empty list notation
+    (" ", "::", " "),  # cons notation
+    ("[", "::", " "),  # singleton list notation
+]
+
+
+def is_excepted(m):
+    for e in EXCEPTIONS:
+        if isinstance(e, str):
+            if m[2] == e:
+                return True
+        elif isinstance(e, tuple):
+            prefix, match, suffix = e
+            if m[2] != match:
+                continue
+            s, e = m.span(2)
+            if prefix != m.string[s - len(prefix) : s]:
+                continue
+            if suffix == m.string[e : e + len(suffix)]:
+                return True
+
+    return False
+
+
+def lint_file(opts, fpath):
+    issues = []
+
+    with fpath.open() as f:
+        src = f.read()
+
+    comments = Comments(src)
+    lineno = LineNumbers(src)
+
+    def violations_of(regex):
+        return (m for m in regex.finditer(src) if m.span() not in comments)
+
+    for i, (rule, msg, shift) in enumerate(ISSUES):
+        for m in violations_of(rule):
+            if is_excepted(m):
+                continue
+            rule = f" [rule: {i + 1}]" if opts.show_rule_number else ""
+            issues.append(
+                (
+                    m.span(2),
+                    f"{fpath}:{lineno[m.span(2)[0]]}: coding style{rule}: {msg}",
+                    shift,
+                )
+            )
+
+    for i, ((s, e), msg, offset_shift) in enumerate(sorted(issues)):
+        print(msg, file=sys.stderr)
+        if opts.explain:
+            line = lineno.line_for_offset(s)
+            print()
+            indent = "  "
+            print(f"{indent}{line}", end="")
+            space = " " * lineno.offset_within_line(s)
+            marker = "^" * (e - s)
+            print(f"{indent}{space}{marker}")
+            print()
+
+        if opts.open_editor:
+            cmd = (
+                opts.open_editor.replace("$EDITOR", os.getenv("EDITOR", "emacsclient"))
+                .replace("$FILE", str(fpath))
+                .replace("$LINE", str(lineno[s]))
+                .replace(
+                    "$OFFSET", str(1 + lineno.offset_within_line(s) + offset_shift)
+                )
+            )
+            try:
+                print(f"-> opening editor by running: {cmd} ...")
+                subprocess.run(cmd, shell=True, check=True)
+            except subprocess.CalledProcessError as e:
+                print("Error: failed to launch {cmd}: {e}", file=sys.stderr)
+
+        if opts.max_issues_per_file is not None and i + 1 >= opts.max_issues_per_file:
+            break
+
+    return len(issues)
+
+
+DEFAULT_OPEN_EDITOR_CMD = "$EDITOR +$LINE:$OFFSET $FILE"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="check for common Prosa coding style violations"
+    )
+
+    parser.add_argument(
+        "input_files",
+        nargs="*",
+        type=Path,
+        metavar="Gallina-file",
+        help="input Gallina files (*.v)",
+    )
+
+    parser.add_argument(
+        "-r",
+        "--show-rule-number",
+        action="store_true",
+        help="State which rule is being violated.",
+    )
+
+    parser.add_argument(
+        "-e",
+        "--explain",
+        action="store_true",
+        help="Illustrate the issue.",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--open-editor",
+        action="store",
+        nargs="?",
+        metavar="COMMAND",
+        const=DEFAULT_OPEN_EDITOR_CMD,
+        help=f"open the file in an editor with the given command (default: {DEFAULT_OPEN_EDITOR_CMD})",
+    )
+
+    parser.add_argument(
+        "-m",
+        "--max-issues-per-file",
+        action="store",
+        nargs="?",
+        metavar="N",
+        type=int,
+        const=1,
+        help="Report at most N issues per file (default: 1).",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    opts = parse_args()
+
+    issues = 0
+    for f in opts.input_files:
+        try:
+            issues += lint_file(opts, f)
+        except OSError as e:
+            print(e, file=sys.stderr)
+            issues += 1
+
+    sys.exit(issues > 0)
+
+
+if __name__ == "__main__":
+    main()
